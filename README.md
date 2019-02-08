ad0e040f550d5651f53e2c412ca087b5b573e043
Abhinay <abhinayreddy1278@gmail.com>
2019-02-08 16:41:19 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Quiz
+# Application Is
+  My app is about calculating marks.
+
+  unit test is on biggest of two numbers
+![screenshot 7](https://user-images.githubusercontent.com/26762371/52510064-325a3a80-2bc0-11e9-88cc-ed00d02c9ac3.png)
--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -5,10 +5,33 @@ namespace UnitTests
 {
     public class UnitTest1
     {
-        [Fact]
+       [Fact]
         public void Test1()
         {
-
+            var expected = 10;
+            var a = 2;
+            var b = 10; 
+            var actual = biggestOfnum(a, b);
+            Assert.Equal(expected,actual);
         }
+        [Theory]
+        [InlineData(10,3,10)]
+        [InlineData(5,5,2)]
+        [InlineData(9,9,1)]
+        public void biggestOfnum_multipleInput(int expected, int n1,int n2){
+            Assert.Equal(expected,biggestOfnum(n1,n2));
+        }
+       
+         
+
+
+        // for more see 
+
+        int biggestOfnum(int x, int y)
+        {
+            return Math.Max(x,y);
+        }
+
+
     }
 }
